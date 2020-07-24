e52d0c04e0afd2584dfdb7bbbe45a588c45bec6c
anirudh-g99 <anirudh.600@gmail.com>
2020-07-24 17:44:18 +0530

    Implement server side registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-
+const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const config = require('config');
 // @route GET api/users
 // @desc Test route
 // @access Public
@@ -12,12 +16,62 @@ router.post(
 		check('email', 'Please enter a valid email').isEmail(),
 		check('password', 'Please enter min 6 characters').isLength({ min: 6 })
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		res.send('User route');
+
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'user already exists' }] });
+			}
+
+			// Get pfp for new user
+			const avatar = gravatar.url(email, {
+				s: '200', //default size
+				r: 'pg', //pg rating
+				d: 'mp' //use default pic if not provided
+			});
+
+			// Create new user
+			user = new User({
+				name,
+				email,
+				avatar,
+				password
+			});
+
+			// Encrypt password
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			await user.save(); // commit
+
+			// Return JWT
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 36000 }, // TODO: change to 3600
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
 	}
 );
 
