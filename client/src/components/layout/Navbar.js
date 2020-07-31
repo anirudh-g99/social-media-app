e87a2ab264eaf58d6a291aea5e14cacbfaf20224
anirudh-g99 <anirudh.600@gmail.com>
2020-07-31 15:04:10 +0530

    Update app name

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav className="navbar bg-dark">
             <h1>
                 <Link to="/">
-                    <i className="fas fa-code"/> Swift
+                    <i className="fas fa-code"/> SwiftLink
                 </Link>
             </h1>
             <ul>
