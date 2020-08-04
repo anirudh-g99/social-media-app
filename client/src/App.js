be996114a8a560a71cf0779832834b2ffce2af9d
anirudh-g99 <anirudh.600@gmail.com>
2020-08-04 23:50:36 +0530

    Implement client side login and registration & refactor code

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,11 @@ const App = () => (
 	<Router>
 		<Fragment>
 			<Navbar />
-			<Route exact path="/" component={Landing} />
-			<section className="container">
+			<Route exact path='/' component={Landing} />
+			<section className='container'>
 				<Switch>
-					<Route exact path="/register" component={Register} />
-					<Route exact path="/login" component={Login} />
+					<Route exact path='/register' component={Register} />
+					<Route exact path='/login' component={Login} />
 				</Switch>
 			</section>
 		</Fragment>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
-		<nav className="navbar bg-dark">
+		<nav className='navbar bg-dark'>
 			<h1>
-				<Link to="/">
-					<i className="fas fa-code" /> SwiftLink
+				<Link to='/'>
+					<i className='fas fa-code' /> SwiftLink
 				</Link>
 			</h1>
 			<ul>
 				<li>
-					<Link to="!#">Developers</Link>
+					<Link to='!#'>Developers</Link>
 				</li>
 				<li>
-					<Link to="/register">Register</Link>
+					<Link to='/register'>Register</Link>
 				</li>
 				<li>
-					<Link to="/login">Login</Link>
+					<Link to='/login'>Login</Link>
 				</li>
 			</ul>
 		</nav>
