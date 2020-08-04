6ba95900a81d40332f09c946c7463ecff8ec1098
anirudh-g99 <anirudh.600@gmail.com>
2020-08-04 23:07:53 +0530

    Prettier formatting changes

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 const Register = () => {
-    return (
-        <div>
-            <h1>Register</h1>
-        </div>
-    )
-}
-export default Register
+	return (
+		<div>
+			<h1>Register</h1>
+		</div>
+	);
+};
+export default Register;
